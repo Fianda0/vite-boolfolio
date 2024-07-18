83eb507307db1a53604014c4dedf6e7ae0ad0263
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-07-18 15:13:47 +0200

    Chiamata axios su api database

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      url: 'http://localhost:8000/',
+      projects: ''
+    }
+  },
+
+  mounted() {
+    axios.get(this.url + 'api/projects').then(response => {
+      console.log(response.data);
+      this.projects = response.data.projects
+      console.log('progetti' + this.projects);
+
+    })
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <p v-for="progetto in this.projects.data">{{ progetto.titolo }}</p>
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
