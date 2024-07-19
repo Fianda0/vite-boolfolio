f26346cdd4364e6cc6b473fadb88dba31f78a6bb
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-07-19 14:42:40 +0200

    create route home

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ export default {
 <template>
 
   <PageCard />
+  <router-view></router-view>
 
 </template>
 
---
+++ src/View/AppHome.vue
@@ -0,0 +1,15 @@
+<script>
+
+export default {
+    name: 'AppHome'
+}
+
+</script>
+
+<template>
+
+    <h1>Apphome</h1>
+
+</template>
+
+<style></style>
