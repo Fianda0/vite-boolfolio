e3c11a21f34f6d740b9b370dbd5abafc19f1ee37
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-07-22 16:02:24 +0200

    add singolCard components

---
+++ src/View/SingolCard.vue
@@ -0,0 +1,26 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'SingolCard',
+    data() {
+        return {
+            base_url: 'http://localhost:8000/',
+            projects: ''
+        }
+    },
+    mounted() {
+        const url = `${this.base_url}/api/projects/${this.$route.params.id}`
+        axios.get(url).then(response => {
+            console.log(response.data);
+            // this.projects = response.data.projects
+            // console.log('progetti' + this.projects);
+
+        })
+    }
+}
+</script>
+
+<template></template>
+
+<style></style>
