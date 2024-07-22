3323f155a05e297596e00d58d1b7621592562f95
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-07-22 16:14:06 +0200

    fix singol card

--- src/View/SingolCard.vue
+++ src/View/SingolCard.vue
@@ -6,21 +6,41 @@ export default {
     data() {
         return {
             base_url: 'http://localhost:8000/',
-            projects: ''
+            project: ''
         }
     },
     mounted() {
-        const url = `${this.base_url}/api/projects/${this.$route.params.id}`
+        const url = `${this.base_url}api/projects/${this.$route.params.id}`
         axios.get(url).then(response => {
             console.log(response.data);
-            // this.projects = response.data.projects
-            // console.log('progetti' + this.projects);
-
+            this.project = response.data.project
         })
     }
 }
 </script>
 
-<template></template>
+<template>
+    <div v-if="project">
+        <div class="container">
+            <div class="row">
+                <div class="col-8">
+                    <h1>{{ project.titolo }}</h1>
+                </div>
+                <div class="col-8">
+                    <div v-if="project.immagine.startsWith('http')">
+                        <img :src="project.immagine" class="card-img-top" alt="">
+                    </div>
+                    <div v-else>
+
+                        <img :src="url + 'storage/' + project.immagine" class="card-img-top" alt="">
+                    </div>
+                </div>
+                <div class="col-8">
+                    <p>Descrizione: {{ project.descrizione }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
 
 <style></style>
